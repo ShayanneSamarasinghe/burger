105bb7bb298a973174dc0269928180081aa9ba8b
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-26 19:23:43 -0400

    Added burger.add to router function

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,6 +5,8 @@ const burger = require("../models/burger.js")
 function router (app){
     app.post("/", function (req,res){
         console.log.apply(`[POST] we received this data:`, req.body )
+        burger.add(req.body.burger)
+        
         res.redirect("/");
     })
     
