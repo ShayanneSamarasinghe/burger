4bfbe12602c9513baccf8d6c1449260435dc684a
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-26 17:00:24 -0400

    Tweaked server.js

--- server.js
+++ server.js
@@ -4,23 +4,24 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const burger = require("./models/burger")
 const routes = require("./controllers/burger_controller.js")
+
 const app = express()
-
-
+// handlebar initilization 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
-
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
 app.use (express.static("public"))
 
-app.use(routes)
+// app.use(routes)
 
 app.listen(PORT, function(){
     console.log(`Listening on port:${PORT}`)
