ffdb8738e1f9e1e86f18b7a92013d2cdc35c2a58
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-26 15:30:42 -0400

    More additions to server.js

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
+const express = require("express")
+const exphbs = require("express-handlebars")
 const orm = require("./models/orm")
+const routes = require("./controllers/burgerControllers.js")
+const app = express()
 
-const app = express();
 
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use (express.static("public"))
+
+app.use(routes)
